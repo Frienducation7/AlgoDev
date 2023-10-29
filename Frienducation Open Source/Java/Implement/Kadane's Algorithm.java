d122c134eac2869695baf985b7c4c45ccdd5a298
Arya Singh <aaryasingh1602@gmail.com>
2023-10-29 17:47:21 +0530

    implemented Kadane's algorithm

--- Frienducation Open Source/Java/Implement/Kadane's Algorithm.java
+++ Frienducation Open Source/Java/Implement/Kadane's Algorithm.java
@@ -1,1 +1,15 @@
-
+public static void Kadanes(int numbers[])
+{
+    int ms=Integer.MIN_VALUE;
+    int cs=0;
+    for(int i=0;i<numbers.length;i++)
+    {
+        cs=cs+numbers[i];
+        ms=Math.max(cs,ms);
+        if(cs<0)
+        {
+            cs=0;
+        }
+    }
+    System.out.println(ms);
+}
