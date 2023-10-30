2187f48161d4b9f6d46eba13cd4e58fbcc7fa57b
Arya Singh <aaryasingh1602@gmail.com>
2023-10-30 08:47:04 +0530

    optamized subsequence problem

--- Frienducation Open Source/Java/Implement/Print all  subsequences.java
+++ Frienducation Open Source/Java/Implement/Print all  subsequences.java
@@ -1,15 +1,12 @@
-public static void subSequences(int arr[])
-  {   int nosub=0;
-	  for(int i=0;i<arr.length;i++)
-	  {
-		  for(int j=i;j<arr.length;j++)
-		  {
-			  for(int k=i;k<=j;k++)
-			  {
-				  System.out.print(arr[k]+" ");
-			  }nosub++;
-			  System.out.println();
-		  }System.out.println();
-	  }
-	  System.out.println(nosub);//Number of sub arrays.
-  }
+public static void subSequences(int arr[]) {
+    int n = arr.length;
+
+    for (int i = 0; i < n; i++) {
+        for (int j = i; j < n; j++) {
+            for (int k = i; k <= j; k++) {
+                System.out.print(arr[k] + " ");
+            }
+            System.out.println();
+        }
+    }
+}
