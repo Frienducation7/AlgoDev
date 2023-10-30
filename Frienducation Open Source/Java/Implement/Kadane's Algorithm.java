30e79a0252c42b7cdde9ef475bc61c2b50fc8590
Arya Singh <aaryasingh1602@gmail.com>
2023-10-30 10:17:47 +0530

    Kadane's algorithm java

--- Frienducation Open Source/Java/Implement/Kadane's Algorithm.java
+++ Frienducation Open Source/Java/Implement/Kadane's Algorithm.java
@@ -1,1 +1,14 @@
-
+public static void kadaneMaxSum(int arr[])
+  {   int cs=0,ms=Integer.MIN_VALUE;
+	  for(int i=0;i<arr.length;i++)
+	  {
+		 cs+=arr[i];
+		
+		 ms=Math.max(ms,cs);
+		 if(cs<0)
+		 {
+			 cs=0;
+		 }
+	  }
+	  System.out.println(ms);
+  }
